9c92b4c706aca964d70d88f97932e4abf88f34ce
unknown <nguyenchungtb99@gmail.com>
2021-11-25 00:22:01 +0700

    update

--- IoT/Arduino_ESP/Arduino_ESP.ino
+++ IoT/Arduino_ESP/Arduino_ESP.ino
@@ -1,3 +1,8 @@
+/*
+ * author: Nguyễn Thành Chung
+ * date: 14/10/2021
+*/
+
 #include <DHT.h>
 //#include <ArduinoJson.h>
 
@@ -8,18 +13,22 @@
 #define DHTTYPE DHT11 // DHT 11
 const int DHTPin = 5;
 
-#define led1 D3
+//#define led1 D3
+#define led 15 // D8
+#define fan 4 // D2
+#define rain 13 // D7
+#define soil 12 // D6
 
 DHT sensor(DHTPin, DHTTYPE);
 
 
-const char* ssid = "Tungduong";
-const char* password = "duong123";
+const char* ssid = "Chung";
+const char* password = "123456789";
+
 
 const char* mqtt_server = "broker.hivemq.com";
-const char* mqtt_username = "duongserver@!#";
-const char* mqtt_password = "duong123";
-
+const char* mqtt_username = "NguyenChung";
+const char* mqtt_password = "chung0609";
 
 WiFiClient mangcb;
 PubSubClient client(mangcb);
@@ -72,68 +81,132 @@ void reconnect() {
   }
 }
 
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  //-----------------------------------------------------------------
 
-// test json 
-//char temp = 'on';
-void callback(char* topic, byte* payload, unsigned int length) {
- Serial.print("nhan: ");
- Serial.println(topic);
- for(int i=0;i<length;i++){
-  Serial.print((char) payload[i]);
- }
- if(char(payload[0]) == '1'){
-  digitalWrite(led1, 0);
-  Serial.print("\n bat");
- }else {
-    digitalWrite(led1, 1);
-    Serial.println("tat");
- }
- 
+  Serial.print("Co tin nhan moi tu topic: ");
+  Serial.println(topic);
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  String message(p);
+  
+ if(String (topic) == "device"){
+    if (message == "led on") {
+    digitalWrite(led, HIGH);
+    } 
+    else if(message == "led off"){
+      digitalWrite(led, LOW);
+    }
+    if (message == "fan on") {
+    digitalWrite(fan, HIGH);
+    } 
+    else if(message == "fan off"){
+      digitalWrite(fan, LOW);
+    }
+  }
+  
+  Serial.println(message);
+  //Serial.write(payload, length);
+  Serial.println();
+  //-------------------------------------------------------------------------
 }
 
-  
-
-
 void setup() {
-  pinMode(led1, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(115200);
+  pinMode(led, OUTPUT);     
+  pinMode(fan, OUTPUT);
+  pinMode(rain, INPUT);
+  pinMode(soil, INPUT);
+  
+  Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
   reconnect();
 
-  client.subscribe("duong/led_status");
+  client.subscribe("device");
 
   sensor.begin();
   
 }
 
+
+int value = NULL;
 void loop() {
  
  int humidity = sensor.readHumidity();
  int temperature = sensor.readTemperature();
+ char device[10][10] = {"", "led on", "led off", "fan on", "fan off"};
+ while(Serial.available()){
+    value = Serial.read();
+  }
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
+  
   if ((unsigned long)(millis() - previousMillis) >= interval) {
     previousMillis = millis();
+    
+  //................DHT11.........................  
     char tempString[8];
-  sprintf(tempString, "%d",temperature);
-  Serial.print("  Temperature: ");
-  Serial.print(tempString);
-  client.publish("home/sensors/temperature", tempString);
+    sprintf(tempString, "%d",temperature);
+    Serial.print("  Temperature: ");
+    Serial.print(tempString);
+    client.publish("home/Chung/temp", tempString);
+    
+    char humString[8];
+    sprintf(humString, "%d", humidity);
+    Serial.print("  Humidity: ");
+    Serial.println(humString);
+    client.publish("home/Chung/hum", humString);
+    
+  //................. Rain sensor..................
   
-  char humString[8];
-  sprintf(humString, "%d", humidity);
-  Serial.print("  Humidity: ");
-  Serial.println(humString);
-  client.publish("home/sensors/humidity", humString);
-
-  }
-  client.loop();
+    int value_rain = digitalRead(rain);//Đọc tín hiệu cảm biến mưa
+    if (value_rain == HIGH) { // Cảm biến đang không mưa
+      Serial.println("sunny");
+      client.publish("home/Chung/rain", "sunny");
+    } else {
+      client.publish("home/Chung/rain", "raining");
+      Serial.println("raining");
+    }
+  //................soil sensor.....................
+    int value_soil = analogRead(soil);//Đọc tín hiệu cảm biến mưa
+    char soilString[8];
+    sprintf(soilString, "%d", value_soil);
+    Serial.println(soilString);
+    client.publish("home/Chung/soil", soilString);
+        
+  //................ device contror.................
+    if(value == '1'){
+      digitalWrite(led,HIGH);
+      client.publish("home/Chung/device", device[1]);
+      value = NULL;
+    }
+    else if(value == '0'){
+      digitalWrite(led,LOW);
+      client.publish("home/Chung/device", device[2]);
+      value = NULL;
+    }
+    else if(value == '3'){
+      digitalWrite(fan,HIGH);
+      client.publish("home/Chung/device", device[3]);
+      value = NULL;
+      }
+    else if(value == '2'){
+      digitalWrite(fan,LOW);
+      client.publish("home/Chung/device", device[4]);
+      value = NULL;
+      }
+    else {
+      client.publish("home/Chung/device",device[0]);
+      }
+    }
+    
+    client.loop();
   
 
 }
